4bd63e6456fafb77642b7c4825c47644fb16dace
salinda hulangamuwa <150468057+SalindaHulangamuwa@users.noreply.github.com>
2025-03-04 06:49:55 +0530

    fix: Improve thread retrieval and error handling in thread API endpoints

--- chatApi/api/thread.py
+++ chatApi/api/thread.py
@@ -5,7 +5,7 @@ import uuid
 
 
 def create_thread(thread: ThreadCreate):
-    user=users_collection.find_one({"userId": thread.userId})
+    user=users_collection.find_one({"_id": thread.userId})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
@@ -22,7 +22,7 @@ def create_thread(thread: ThreadCreate):
 
 
 def get_thread(userId: str):
-    threads=threads_collection.find_one({"userId": userId},{"_id":1,"userId":1,"threadHeading":1})
+    threads=threads_collection.find({"userId": userId},{"_id":1,"userId":1,"threadHeading":1})
     if not threads:
         raise HTTPException(status_code=404, detail="Thread not found")
     
@@ -32,8 +32,9 @@ def get_thread(userId: str):
 
 
 def get_thread_by_id(threadId: str):
-    thread = threads_collection.find_one({"_id": threadId}, {"_id": 0,"userId":1,"threadHeading":1})  #First, find the query by threadId, and then project it. Setting _id=0 means the default _id is ignored.
+    thread = threads_collection.find({"_id": threadId}, {"_id":0,"userId":1,"threadHeading":1})  #First, find the query by threadId, and then project it. Setting _id=0 means the default _id is ignored.
     if not thread:
         raise HTTPException(status_code=404, detail="Thread not found")
+    thread_list=list(thread)
 
-    return thread
+    return thread_list
